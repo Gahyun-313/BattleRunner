cd689a25676fc04184cc2cb04b80520a49525882
Gahyun-313 <gus20313@gmail.com>
2024-10-05 23:58:32 +0900

    상태바 색상 통일
    - activity_battle.xml의 배경을 @color/white로 설정
    - 상태바 색상 @color/white로 설정
    -> 색상 통일 해결

--- app/src/main/java/com/example/battlerunner/MainActivity.kt
+++ app/src/main/java/com/example/battlerunner/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.battlerunner
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.battlerunner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +18,9 @@ class MainActivity : AppCompatActivity() {
 
         // 여기서 binding을 바로 초기화
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
+        setContentView(binding.root)
 
     }
 }
+
