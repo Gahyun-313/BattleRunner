26074dc2c4072e47ff6974db082f1026d5635370
hys12 <yooseok21@naver.com>
2024-10-11 18:18:43 +0900

    로그인 구현?
    약간 애매

---
+++ app/src/main/java/com/example/battlerunner/GlobalApplication.kt
@@ -0,0 +1,14 @@
+package com.example.battlerunner
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // 다른 초기화 코드들
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "16005e7a061659542eeaf1f020717f46")
+    }
+}
