ee22a4b1b042cddb6121ced0c89527d947d73968
Gahyun-313 <gus20313@gmail.com>
2024-10-12 00:06:10 +0900

    스플래시 -> 로그인 연결
    [스플래시 화면 추가]
    - 스플래시 화면 2초 나타난 뒤 LoginActivity로 이동
    
    [홈바 + 종료 팝업]
    - 실행 시작 시 프래그먼트, 홈바 네비게이션 모두 개인러닝에서 시작하도록 설정
    - 홈바 네비게이션으로 배틀러닝, 개인러닝, 마이페이지 이동 가능
    - 개인러닝 종료 -> activity_personal_end.xml 팝업 실행(PersonalEndActivity 실행) -> closeBtn 클릭시 액티비티 종료
    - 배틀러닝 종료 -> activity_battle_end.xml 팝업 실행(BattleEndActivity 실행) -> closeBtn 클릭시 액티비티 종료

--- app/src/main/java/com/example/battlerunner/SplashActivity.kt
+++ app/src/main/java/com/example/battlerunner/SplashActivity.kt
@@ -19,10 +19,10 @@ class SplashActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_splash)
 
-        // 3초 후에 MainActivity로 이동
+        // 3초 후에 LoginActivity로 이동
         lifecycleScope.launch {
             delay(2000L) // 2초 지연
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
             finish() // 스플래시 액티비티 종료
         }
